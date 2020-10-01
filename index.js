69548ac254157ff434a6fb790e9fb06d36a1b7aa
Merve <gulineyel@gmail.com>
2020-10-01 18:01:04 +0300

    background changed

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 // Change The Colors!
-
 const colors = [
   {
     name: "red",
@@ -22,39 +21,30 @@ const colors = [
     motto: "Deep Purple",
   },
 ];
-
 const randomColorButton = document.querySelector("#randomColorButton");
 const addColorButton = document.querySelector("#setColorButton");
-
 addColorButton.addEventListener("click", addColor);
 randomColorButton.addEventListener("click", changeColors);
 // randomColorButton.addEventListener("click", () => changeColors());
 // randomColorButton.addEventListener("click", function () {
 //   changeColors();
 // });
-
 // 1) Arrayden rastgele element seçmek
 function changeColors() {
   const randomIndex = Math.floor(Math.random() * colors.length);
-
   document.querySelector("#colorName").textContent = colors[randomIndex].name;
   document.querySelector("#colorDesc").textContent = colors[randomIndex].motto;
-
   document.querySelector("body").style.backgroundColor =
     colors[randomIndex].name;
 }
-
 // 2) Kullanıcının girdiği string'i renk listesine eklemek ve arkaplanı o renk ile güncellemek
 function addColor() {
   const userInput = document.querySelector(".colorInput");
-
   const colorData = userInput.value.split(":");
-
   const colorObject = {
     name: colorData[0],
     motto: colorData[1],
   };
-
   // indexOf SADECE flat array (["red", "blue"] / [1, 2, 3]) geçerlidir.
   // findIndex object arraylerde geçerlidir.
   const searchedColorIndex = colors.findIndex(
@@ -64,7 +54,6 @@ function addColor() {
     document.querySelector("body").style.backgroundColor = colorObject.name;
     document.querySelector("#colorName").textContent = colorObject.name;
     document.querySelector("#colorDesc").textContent = colorObject.motto;
-
     colors.push(colorObject);
     userInput.value = "";
     userInput.focus();
@@ -72,8 +61,6 @@ function addColor() {
     alert("Renk zaten mevcut!");
   }
 }
-
 // Kullanıcı boş bir değer girerse?
 // Kullanıcı geçersiz bir değer girerse?
-
-// Bir input daha ekleyin: kullanıcıdan hex'a değer #eceff1?
+// Bir input daha ekleyin: kullanıcıdan hex'a değer #ECEFF1?
